1a10ac814e9c0d6088a55da680a6abfb1cb84452
Joe Landers <joe@joelanders.net>
2016-05-05 18:59:14 +0200

    template the MSON boilerplate

--- entries/app.md
+++ entries/app.md
@@ -1,13 +1,6 @@
-# GET /
-+ Response 200 (application/json)
-    + Attributes (Signal)
-
-# Data Structures
-
-# Signal
-
 - id (required) - unique identifier
 - name (required) - app name
+- license - license
 - type (required) - app/os/hw/infra (XXX: should be fixed "app" in this doc?)
 - source - URL to source repo
 - tracker - URL to issue tracker
@@ -19,9 +12,3 @@
 - os (required)
 - oses (array[OS], fixed)
 - infra (array[Infra], fixed)
-
-:[](../_meta/dependency.md)
-:[](../_meta/build.md)
-:[](../_meta/os.md)
-:[](../_meta/infra.md)
-:[](../_meta/metadata.md)
--- mson-to-json-schema.sh
+++ mson-to-json-schema.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-set -euv
+set -eu
 
-HERCULE=/usr/bin/hercule
+HERCULE=/usr/local/bin/hercule
 DRAFTER=/usr/local/bin/drafter
 REFRACT_FILTER=/usr/local/bin/refract-filter
 
+path-to-obj-name() {
+    file=$(basename $1) # strips the path part
+    name=${file%.*} # strips the extension
+}
+
+# takes an argument that's the object name like App or Signal
+print_header() { cat <<-EOF
+# GET /
++ Response 200 (application/json)
+    + Attributes (dummy)
+
+# Data Structures
+
+# dummy
+EOF
+}
+
+# we include all the header files in every document
+print_footer() {
+    for f in $( find _meta -name "*.md" ); do
+        printf "\n:[]($f)\n"
+    done
+}
+
 # input (filename) -> hercule -> drafter -> refract-filter -> output (stdout)
 mson-to-json-schema() {
-    $HERCULE $1 \
+    ( print_header $(path-to-obj-name $1) && cat $1 && print_footer ) \
+        | $HERCULE \
         | $DRAFTER --format json \
         | $REFRACT_FILTER -j --content_type="application/schema+json";
 }
@@ -18,9 +43,5 @@ for schema_file in `find entries -type f -name "*.md"`; do
     mson-to-json-schema $schema_file > ${schema_file%.*}.json
 done
 
-find entries
 
-echo $CI_BUILD_REF_NAME
-git status
 
-source commit-json-schema.sh
