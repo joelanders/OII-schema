8a8a103be6da8e87269913ed4923a6ff5cd1c2e4
Jun Matsushita <jun@iilab.org>
2016-08-05 11:56:56 +0200

    Moving notebook to its own subrepo

--- notebooks/plot.js
+++
@@ -1,9 +0,0 @@
-// $$svg$$ = "<svg><rect width=80 height=80 style='fill: orange;'/></svg>";
-
-'use strict'
-
-var plt = require('plotly-notebook-js')
-
-var myPlot = plt.createPlot([{ x: [1,2,3], y: [3,4,5] }], { title: 'Plotly in Jupyter!' });
-
-$$html$$ = myPlot.render();
--- notebooks/test.js
+++
@@ -1,1 +0,0 @@
-$$.html("<div style='background-color:olive;width:50px !important;height:50px'></div>");
